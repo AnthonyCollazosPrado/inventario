667c07ea32eb9de400b7243421e5c049c4cf2f49
unknown <Usuario@EUREKAW10.AGROKASA>
2024-02-23 14:21:38 -0500

    v7

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const info = {
-    direccion: 'https://localhost:7039/api/Inventario',
-    version: '?v=5'
+    //direccion: 'https://localhost:7039/api/Inventario', //[DEV]
+    direccion: 'http://192.168.0.36:7070/api/Inventario', //[QA]
+    version: '?v=7'
 }
 let respuesta;
 let paginaActual = 1;
